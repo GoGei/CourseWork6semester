1c73400625274f405a35b7e08a3091f778765f7d
gogei <GoGei2001Q>
2021-05-27 22:18:04 +0300

    Changed settings

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'testRegistBot@gmail.com'
-EMAIL_HOST_PASSWORD = 'registration_bot_password'
-
-
+EMAIL_HOST_USER = 'realtoragency.coursework@gmail.com'
+EMAIL_HOST_PASSWORD = 'realtoragency.coursework123'
