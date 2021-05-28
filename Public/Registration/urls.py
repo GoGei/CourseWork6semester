332f7376c900b6ac68d7343107d1820e6e77e471
gogei <GoGei2001Q>
2021-05-28 12:41:34 +0300

    Added sorting of table Removed unused files

--- Public/Registration/urls.py
+++ Public/Registration/urls.py
@@ -5,11 +5,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.registration, name='registration'),
-    # url(r'^reset-password-request/$', views.reset_password_request, name='reset-password-request'),
-    # url(r'^reset-password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     views.registration_reset_password, name='reset-password'),
-    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     views.registration_create_password, name='activate'),
-    # url(r'^email-existence/(?P<email>(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})))/$', views.email_existence, name='email-existence'),
-    # url(r'^user-correct/(?P<email>(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})))/$', views.user_correct, name='user-correct'),
 ]
